0956c38211b91f7139042b62ee62d47027f20907
tulikaMithal <tm290392@gmail.com>
2018-11-27 23:33:01 -0600

    Included multipart

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -1,15 +1,29 @@
 package controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -24,7 +38,9 @@ import model.Status;
 /**
  * Servlet implementation class ItemController
  */
+@MultipartConfig
 @WebServlet("/ItemController")
+
 public class ItemController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -46,19 +62,135 @@ public class ItemController extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		ItemDAO itemDao = new ItemDAOImpl();		
-		String submitType = request.getParameter("submit").trim();
+		ItemDAO itemDao = new ItemDAOImpl();
+
+		String submitType = null;
+		String username = null;
+		int itemId = 0;
+		String query = null;
+		int categoryId= 0;
+		String queryAuto = null;
+		String pname=null;
+		String tags = null;
+		int quantity = 0;
+		int id=0;
+		String comment=null;
+		Boolean for_sale = null;
+		Boolean negotiable = null;
+		Boolean status = null;
+		float price = 0;
+		String img_url = null;
 		
+		if(request.getContentType().toLowerCase().indexOf("multipart/form-data") > -1) {
+			List<FileItem> multipart = null;
+			try {
+				multipart = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(new ServletRequestContext(request));
+			} catch (FileUploadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			for (FileItem item : multipart) {
+	            if (!item.isFormField())
+	            {
+	            	String path="C:\\Users\\lenovo\\gitUtdeals\\UTDeal\\WebContent\\home\\assets\\img\\";
+	                
+	            	int index = item.getName().lastIndexOf('\\');
+	            	int len = item.getName().length();
+	                String fileName =item.getName().substring(index+1, len);
+	                
+	                img_url = path+fileName;
+	                FileOutputStream out = null;
+	                
+	                InputStream filecontent = null;
+	                  
+	                out = new FileOutputStream(new File(path + fileName));
+	               
+	                filecontent = item.getInputStream();
+	           
+	       
+		              int read = 0;
+		              final byte[] bytes = new byte[1024];
+		       
+		              while ((read = filecontent.read(bytes)) != -1) {
+		                  out.write(bytes, 0, read);
+		                
+		              }
+		              
+		              
+		              out.close();
+	            }
+	            
+	          
+	            if (item.isFormField()) { 
+	 
+					if (item.getFieldName().equals("submit")) {
+					     submitType = item.getString();
+					}
+					if (item.getFieldName().equals("username")) {
+					     username = item.getString();
+					}
+					if (item.getFieldName().equals("itemId")) {
+					     itemId = Integer.parseInt(item.getString());
+					}
+					if (item.getFieldName().equals("Search")) {
+					     query = item.getString();
+					}
+					if (item.getFieldName().equals("category")) {
+					     categoryId = Integer.parseInt(item.getString());
+					}
+					if (item.getFieldName().equals("q")) {
+					     queryAuto= item.getString();
+					}
+					if (item.getFieldName().equals("id")) {
+					     id = Integer.parseInt(item.getString());
+					}
+					if (item.getFieldName().equals("quantity")) {
+					     quantity = Integer.parseInt(item.getString());
+					}
+					if (item.getFieldName().equals("tags")) {
+					     tags = item.getString();
+					}
+					if (item.getFieldName().equals("pname")) {
+					     pname = item.getString();
+					}
+					if (item.getFieldName().equals("comment")) {
+					     comment = item.getString();
+					}
+					if (item.getFieldName().equals("for_sale")) {
+					     for_sale = Boolean.parseBoolean(item.getString());
+					}
+					if (item.getFieldName().equals("negotiable")) {
+					     negotiable = Boolean.parseBoolean(item.getString());
+					}
+					if (item.getFieldName().equals("status")) {
+					     status = Boolean.parseBoolean(item.getString());
+					}
+					if (item.getFieldName().equals("price")) {
+					     price = Float.parseFloat(item.getString());
+					}
+					
+	            }
+			}
+		}	
+		
+		else {
+			submitType = request.getParameter("submit");
+		}
+	
 		System.out.println("submitType "+submitType);
 		
 		
 		if(submitType.equals("getPostedItems")) {
-			String username = request.getParameter("username");
+		
 			System.out.println("inside if");
 			List<Item> items = itemDao.fetchPostedItems(username);
 			System.out.println("posted items "+items.toString());
@@ -67,7 +199,7 @@ public class ItemController extends HttpServlet {
 		}
 		
 		else if(submitType.equals("getFavoriteItems")) {
-			String username = request.getParameter("username");
+			
 			System.out.println("inside if");
 			List<Item> items = itemDao.fetchFavItems(username);
 			System.out.println("fav items "+items.toString());
@@ -77,21 +209,25 @@ public class ItemController extends HttpServlet {
 		
 		else if(submitType.equals("saveItem")) {
 			Item item = new Item();
-			item.setName(request.getParameter("pname"));
-			item.setCategory(new Category(Integer.parseInt(request.getParameter("category"))));
-			if(request.getParameter("quantity")!=null)item.setQuantity(Integer.parseInt(request.getParameter("quantity")));
-			item.setTags(request.getParameter("tags"));			
-			if(request.getParameter("fav_count")!=null)item.setFav_count(Integer.parseInt(request.getParameter("fav_count")));
+			item.setName(pname);
+			item.setCategory(new Category(categoryId));
+			//if(request.getParameter("quantity")!=null)
+			item.setQuantity(quantity);
+			item.setTags(tags);			
+			//if(request.getParameter("fav_count")!=null)item.setFav_count(Integer.parseInt(request.getParameter("fav_count")));
 			item.setCustomer(new Customer(request.getSession().getAttribute("username").toString()));
-			if(request.getParameter("image_url")!=null)item.setImage_url(request.getParameter("image_url"));
-			item.setComments(request.getParameter("comment"));
-			item.setDate_posted(request.getParameter("date_posted"));
-			if(request.getParameter("for_sale")!=null)item.setFor_sale(Boolean.parseBoolean(request.getParameter("for_sale")));
-			item.setNegotiable(Boolean.parseBoolean(request.getParameter("negotiable")));
-			item.setPrice(Float.parseFloat(request.getParameter("price")));
-			item.setStatus(Boolean.parseBoolean(request.getParameter("status")));
+			//if(request.getParameter("image_url")!=null)
+			int ind = img_url.lastIndexOf("home");
+			item.setImage_url(img_url.substring(ind, img_url.length()));
+			item.setComments(comment);
+			//item.setDate_posted(request.getParameter("date_posted"));
+			//if(request.getParameter("for_sale")!=null)
+			item.setFor_sale(for_sale);
+			item.setNegotiable(negotiable);
+			item.setPrice(price);
+		//	item.setStatus(status);
 			
-			System.out.println("check"+Boolean.parseBoolean(request.getParameter("for_sale")));
+			//System.out.println("check"+Boolean.parseBoolean(request.getParameter("for_sale")));
 			itemDao.saveItem(item);
 //			List<Item> items = itemDao.fetchPostedItems(request.getSession().getAttribute("username").toString());
 //			System.out.println("Getting list of posted items"+items);
@@ -102,9 +238,7 @@ public class ItemController extends HttpServlet {
 		
 		
 		else if(submitType.equals("editItem")) {
-			
-			int itemId = Integer.parseInt(request.getParameter("itemId"));
-			System.out.println("check"+request.getParameter("itemId"));
+
 			Item item = itemDao.getItemDetails(itemId);
 			
 			HashMap<String,String> map = new HashMap<String,String>();
@@ -116,6 +250,7 @@ public class ItemController extends HttpServlet {
 			map.put("comments", item.getComments());
 			map.put("itemId", Integer.toString(item.getId()));
 			map.put("for_sale",Boolean.toString(item.getFor_sale()));
+			map.put("image_url", item.getImage_url());
 			map.put("status", Boolean.toString(item.getStatus()));
 			map.put("negotiable", Boolean.toString(item.getNegotiable()));
 			
@@ -127,19 +262,19 @@ public class ItemController extends HttpServlet {
 		
 		else if(submitType.equals("updateItem")) {
 			Item item = new Item();
-			item.setId(Integer.parseInt(request.getParameter("itemId")));
-			item.setName(request.getParameter("pname"));
-			item.setCategory(new Category(Integer.parseInt(request.getParameter("category"))));
-			if(request.getParameter("quantity")!=null)item.setQuantity(Integer.parseInt(request.getParameter("quantity")));
-			item.setTags(request.getParameter("tags"));			
-			if(request.getParameter("fav_count")!=null)item.setFav_count(Integer.parseInt(request.getParameter("fav_count")));
+			item.setId(itemId);
+			item.setName(pname);
+			item.setCategory(new Category(categoryId));
+			item.setQuantity(quantity);
+			item.setTags(tags);	
 			item.setCustomer(new Customer(request.getSession().getAttribute("username").toString()));
-			if(request.getParameter("image_url")!=null)item.setImage_url(request.getParameter("image_url"));
-			item.setComments(request.getParameter("comment"));
-			if(request.getParameter("for_sale")!=null)item.setFor_sale(Boolean.parseBoolean(request.getParameter("for_sale")));
-			item.setNegotiable(Boolean.parseBoolean(request.getParameter("negotiable")));
-			item.setPrice(Float.parseFloat(request.getParameter("price")));
-			item.setStatus(Boolean.parseBoolean(request.getParameter("status")));
+			int ind = img_url.lastIndexOf("home");
+			item.setImage_url(img_url.substring(ind, img_url.length()));
+			item.setComments(comment);
+			item.setFor_sale(for_sale);
+			item.setNegotiable(negotiable);
+			item.setPrice(price);
+			item.setStatus(status);
 			
 			System.out.println(item.toString());
 			itemDao.updateItem(item);
@@ -147,14 +282,14 @@ public class ItemController extends HttpServlet {
 		}
 		
 		else if(submitType.equals("deleteItem")) {
-			int status = itemDao.deleteItem(Integer.parseInt(request.getParameter("id")));
+			int stat = itemDao.deleteItem(id);
 			request.getRequestDispatcher("userhomepage.jsp").forward(request, response);
 		}
 		
 		else if(submitType.equals("addToFavorites")) {
 			System.out.println("add to favorites controller");
 			Item item = new Item();
-			item.setId(Integer.parseInt(request.getParameter("itemId")));
+			item.setId(itemId);
 			//item.setId(366);
 //			item.setName(request.getParameter("pname"));
 //			item.setCategory(new Category(Integer.parseInt(request.getParameter("category"))));
@@ -176,10 +311,9 @@ public class ItemController extends HttpServlet {
 		
 		else if(submitType.equals("autocomplete")){
 
-			int categoryId = Integer.parseInt(request.getParameter("category"));
-			String query = request.getParameter("q");
-			System.out.println(categoryId+" "+query);
-			List<Item>  res = itemDao.getAutoComplete(query, categoryId);
+			queryAuto = request.getParameter("q");
+			categoryId = Integer.parseInt(request.getParameter("category"));
+			List<Item>  res = itemDao.getAutoComplete(queryAuto, categoryId);
 			Gson gson = new Gson();
 			String element = gson.toJson(
                     res,new TypeToken<ArrayList<Item>>() {}.getType());
@@ -191,7 +325,6 @@ public class ItemController extends HttpServlet {
 		
 		else if(submitType.equals("searchItem")){
 
-			String query = request.getParameter("Search");
 			System.out.println("query"+query);
 			List<Item>  res = itemDao.getSearchedItems(query);
 			System.out.println("query"+res.size());
@@ -203,9 +336,7 @@ public class ItemController extends HttpServlet {
 		}
 		
 		else {
-			
-			int itemId = Integer.parseInt(request.getParameter("itemId"));
-			System.out.println("check"+request.getParameter("itemId"));
+		
 			Item item = itemDao.getItemDetails(itemId);
 			
 			HashMap<String,String> map = new HashMap<String,String>();
@@ -215,6 +346,7 @@ public class ItemController extends HttpServlet {
 			map.put("tags", item.getTags());
 			map.put("price", Float.toString(item.getPrice()));
 			map.put("comments", item.getComments());
+			map.put("image_url", item.getImage_url());
 			map.put("itemId", Integer.toString(item.getId()));
 			map.put("for_sale",Boolean.toString(item.getFor_sale()));
 			map.put("status", Boolean.toString(item.getStatus()));
